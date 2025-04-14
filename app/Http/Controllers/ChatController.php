90023e286b09a9a7d80019d1b7bea77e55eecce2
StevenCesen <steven.cesen@unl.edu.ec>
2025-04-14 18:18:14 -0500

    Se separó EndPoint para buscar y descargar un chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -87,7 +87,7 @@ class ChatController extends Controller
         $invoice=Pdf::loadView('ride',$data);
 
         //$filename=Contact::where('id',$id->contact_id)->first()->name.'_'.Contact::where('id',$id->contact_id)->first()->phone_number.'.pdf';
-        $filename="Chat.pdf";
+        $filename="Chat".(Contact::where('id',$id->contact_id)->first()->name).".pdf";
         return $invoice->download($filename);
     }
 
