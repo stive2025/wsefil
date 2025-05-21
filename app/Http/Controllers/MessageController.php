75119bc3bb458facc9d8ba60de95bd47166cca72
StevenCesen <steven.cesen@unl.edu.ec>
2025-05-21 00:24:13 -0500

    Se cambia el formato del audio

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -82,7 +82,7 @@ class MessageController extends Controller
                     $name=date('H_i_s',time()-18000);
 
                     if($file->type=='audio'){
-                        $format='webm';
+                        $format='wav';
                     }
 
                     $stream=base64_decode($file->media);
