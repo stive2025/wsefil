8c8ee3efa4175bd6107e9debdd15cfe5cd95ca09
StevenCesen <steven.cesen@unl.edu.ec>
2025-06-18 03:23:09 -0500

    Se agrego description en TAGS y se implementó la asociación de user_id automáticamente :)

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -316,6 +316,7 @@ class MessageController extends Controller
             'media_path'=>($request->media_type!='chat') ? $filename : ""
         ],200);
     }
+    
 
     /**
      * Display the specified resource.
